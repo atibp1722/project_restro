2b12587dcaa700ea116b4b57452afde9db5f8611
atibp1722 <atibp1722@gmail.com>
2023-04-28 13:42:40 +0545

    customer login and registration form and feature added into accounts folder

--- project_restro/project_restro/settings.py
+++ project_restro/project_restro/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app_restro'
+    'app_restro',
+    'accounts'
 ]
 
 MIDDLEWARE = [
