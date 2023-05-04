81af26db9c8be887d70a69ffc740278e612d6eea
atibp1722 <atibp1722@gmail.com>
2023-05-04 15:13:34 +0545

    added search and filter feature on the admin dashboard

--- project_restro/app_restro/admin.py
+++ project_restro/app_restro/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 from .models import Category, MenuModel
 
 # Register your models here.
-admin.site.register(Category)
-admin.site.register(MenuModel)
+
+class AdminMenu(admin.ModelAdmin):
+    list_display = ('menu_title', 'category_id', 'menu_price')
+    search_fields=('menu_title', )
+    list_filter = ('menu_price', )
+
+class AdminCategory(admin.ModelAdmin):
+    list_display = ('category_name', )
+
+admin.site.register(Category, AdminCategory)
+admin.site.register(MenuModel, AdminMenu)
