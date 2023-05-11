998893300a4bec80fbd9305f0ff3588abb4a3061
atibp1722 <atibp1722@gmail.com>
2023-05-11 07:41:52 +0545

    Category and menu api created to get data and view data from postman

--- project_restro/restapi/urls.py
+++ project_restro/restapi/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import CategoryApiView, MenuApiView
 
 urlpatterns=[
-    
+    path('category/',CategoryApiView.as_view()),
+    path('menu/',MenuApiView.as_view())
 ]
