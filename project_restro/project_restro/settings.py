2af23e5cbb8b923f15481be0849974ef7950ee64
Atib Pokharel <atibp1722@gmail.com>
2023-07-06 16:46:45 +0545

    Update settings,py

--- project_restro/project_restro/settings.py
+++ project_restro/project_restro/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+wugu=_5p67o^l8t6i7r+t*4yye$f%h$jxjzr_(c26&9#a+x_^'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
